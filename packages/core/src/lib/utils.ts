1bc3b8cc12e35bfc23e4ff0e257b3fb3dbb6bc2d
Georgii Bukharov <souljorje@gmail.com>
2026-02-23 20:46:31 +0400

    ignore non-function methods in applyModifiers

--- packages/core/src/lib/utils.ts
+++ packages/core/src/lib/utils.ts
@@ -161,6 +161,7 @@ export function applyModifiers<T extends object>(
     if (!isDefined(methodArgs)) continue
 
     const fn = target[methodName] as AnyFn
+    if (!isFunction(fn)) continue
     const normalizedArgs = Array.isArray(methodArgs) ? methodArgs : [methodArgs]
     fn.apply(target, normalizedArgs)
   }
